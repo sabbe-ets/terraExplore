e18a562d1117f9244d94cc059c33f438492f2495
sabbe-ets <abbe@asu.edu>
2022-06-28 15:13:53 -0700

    v .2

--- main.tf
+++ main.tf
@@ -5,8 +5,27 @@ provider "aws" {
 resource "aws_instance" "test" {
 ami = "ami-098e42ae54c764c35"
 instance_type = "t2.micro"
+vpc_security_group_ids = [aws_security_group.instance.id]
+
+user_data = <<-EOF
+            #!/bin/bash
+            echo "hello world" >index.html
+            nohub busybox httpd -f -p 8080 &
+            EOF
+
+resource "aws_security_group" "instance" {
+    name = "sandbox-example-instance"
+    ingress { 
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
 
 tags = {
     Name = "sandbox test"
+    Acct = "scottsand"
+    Nonsense = "you don't have to put out the red light"
     }
 }
