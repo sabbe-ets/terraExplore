bb8baf767c8aa88e96aedb24471b963cb3a2bc73
sabbe-ets <abbe@asu.edu>
2022-07-18 15:11:09 -0700

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,45 @@
 provider "aws" {
- region = "us-west-2"
+  region = "us-west-2"
 }
 
 resource "aws_instance" "test" {
-ami = "ami-098e42ae54c764c35"
-instance_type = "t2.micro"
-vpc_security_group_ids = [aws_security_group.instance.id]
+  ami                    = "ami-098e42ae54c764c35"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
-user_data = <<-EOF
+  user_data = <<-EOF
             #!/bin/bash
             echo "hello world" >index.html
-            nohub busybox httpd -f -p 8080 &
+            nohub busybox httpd -f -p ${var.server_port} &
             EOF
-tags = {
-    Name = "sandbox test"
-    Acct = "scottsand"
+
+# interpolation ${...} replaced 8080  
+
+  tags = {
+    Name     = "sandbox test"
+    Acct     = "scottsand"
     Nonsense = "you don't have to put out the red light"
-    }
+  }
 }
 resource "aws_security_group" "instance" {
-    name = "sandbox-example-instance"
-    ingress { 
-        from_port = 8080
-        to_port = 8080
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  name = "sandbox-example-instance"
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
+# variable reference (line 24 and 25)
+variable "server_port" {
+    description = "port server will use for http requests"
+    type        = number
+    default     = 8080  
+}
+
+# ouput variable
+output "public_ip" {
+    value       = aws_instance.test.public_ip
+    description = "the public IP of this test" 
+}
